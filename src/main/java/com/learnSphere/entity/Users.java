4e7dcadd9ac4c9ca7c5307c312ab4ed3fd78a3c3
fhirdosbasha <143482043+fhirdosbasha@users.noreply.github.com>
2023-08-29 13:20:58 +0530

    Add files via upload

---
+++ src/main/java/com/learnSphere/entity/Users.java
@@ -0,0 +1,131 @@
+package com.learnSphere.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	String fname;
+	String lname;
+	String email;
+	String password;
+	String role;
+	String mobile;
+	@OneToMany 
+	List<Course> courses;
+	
+	public Users() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Users(int id, String fname, String lname, String email, String password, String role, String mobile,
+			List<Course> courses) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.mobile = mobile;
+		this.courses = courses;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFname() {
+		return fname;
+	}
+
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+
+	public String getLname() {
+		return lname;
+	}
+
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public String getMobile() {
+		return mobile;
+	}
+
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", password="
+				+ password + ", role=" + role + ", mobile=" + mobile + ", courses=" + courses + "]";
+	}
+
+}
